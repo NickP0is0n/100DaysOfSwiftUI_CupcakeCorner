4cded1b3e9be06fb94b3bec65ea6b53d9360ff8e
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-10-03 17:59:50 +0300

    Complex haptics demo

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -6,42 +6,47 @@
 //
 
 import SwiftUI
-
-class User: Codable, ObservableObject {
-    enum CodingKeys: String, CodingKey {
-        case _name = "name"
-    }
-    
-    @Published var name = "Taylor"
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(name, forKey: ._name)
-    }
-    
-    init() {
-        
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        name = try(container.decode(String.self, forKey: ._name))
-    }
-}
+import CoreHaptics
 
 struct ContentView: View {
+    
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Button("Encode Taylor") {
-            encodeTaylor()
+        Button("Play haptic") {
+            complexSuccess()
+        }.onAppear(perform: prepareHaptics)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating an engine – \(error.localizedDescription)")
         }
     }
     
-    func encodeTaylor() {
-        let data = try! JSONEncoder().encode(User())
-        let str = String(decoding: data, as: UTF8.self)
-        print(str)
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness ], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern – \(error.localizedDescription)")
+        }
     }
 }
 
