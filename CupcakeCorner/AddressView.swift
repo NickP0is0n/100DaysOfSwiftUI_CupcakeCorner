38c5a830a40acc8e5d11f373ffeec4f790c292a5
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-10-04 14:05:35 +0300

    Delivery details

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,38 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Mykola Chaikovskyi on 03.10.2024.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView(order: Order())
+    }
+}
